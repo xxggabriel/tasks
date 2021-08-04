20da7ba49a4e550a62c2225d39a09de82c3bb0ea
Gabriel Oliveira De Souza <gabriel@umobi.com.br>
2021-08-04 08:57:40 -0300

    Modificações visuais

--- script/script.js
+++ script/script.js
@@ -13,111 +13,7 @@
     firebase.initializeApp(firebaseConfig);
 })()
 
-// const user_database = {};
-// let user_id = false;
-// (function () {
-
-//     function init_user(userId) {
-//         user_database.user_id = userId
-//     }
-
-//     function new_user(name, admin) {
-//         const user_data = {
-//             userName: name,
-//             admin: admin,
-//             createdat: firebase.database.ServerValue.TIMESTAMP,
-//         }
-
-//         if (!user_database.user_id) {
-//             user_database.user_id = firebase.database().ref().child('users').push().key
-//         }
-
-//         let updates = {};
-//         updates['/users/' + user_database.user_id] = user_data;
-
-//         let user_ref = firebase.database().ref();
-//         user_ref.update(updates)
-//             .then(function () {
-//                 return {
-//                     success: true,
-//                     message: 'Usuário criado.'
-//                 };
-//             })
-//             .catch(function (error) {
-//                 console.log(error.message);
-//                 return {
-//                     success: false,
-//                     message: 'Erro ao criar usuário'
-//                 }
-//             });
-//     }
-
-//     function user() {
-//         if (!user_database.user_id) return { success: false, message: 'Usuário inválido' }
-
-//         let user_ref = firebase.database().ref('/users/' + user_database.user_id);
-
-//         return user_ref.get().then((snapshot) => {
-//             if (snapshot.exists()) {
-//                 return snapshot.val()
-//             } else {
-//                 return { success: false, message: "Usuário não existe." }
-//             }
-//         }).catch((error) => {
-//             if (!user_database.user_id) return { success: false, message: error.message }
-//         });
-//     }
-
-//     function edit_user(user) {
-//         if (!user_database.user_id) return { success: false, message: 'Usuário inválido' }
-
-//         let user_ref = firebase.database().ref('/users/' + user_database.user_id);
-
-//         user_ref.set(user)
-//             .then(function () {
-//                 return {
-//                     success: true,
-//                     message: 'Usuário atualizado.'
-//                 };
-//             })
-//             .catch(function (error) {
-//                 console.log(error.message);
-//                 return {
-//                     success: false,
-//                     message: 'Erro ao atualizar o usuário'
-//                 }
-//         });
-//     }
-
-//     function remove_user() {
-//         if (!user_database.user_id) return { success: false, message: 'Usuário inválido' }
-
-//         let user_ref = firebase.database().ref('/users/' + user_database.user_id);
-//         user_ref.remove()
-//             .then(function () {
-//                 return {
-//                     success: true,
-//                     message: 'Usuário excluido com sucesso.'
-//                 };
-//             })
-//             .catch(function (error) {
-//                 console.log(error.message);
-//                 return {
-//                     success: false,
-//                     message: 'Erro ao criar usuário'
-//                 }
-//         });
-//     }
-
-
-
-//     user_database.user_id = user_id;
-//     user_database.init = init_user;
-//     user_database.new = new_user;
-//     user_database.user = user;
-//     user_database.edit = edit_user
-//     user_database.remove = remove_user;
-// })()
+painelUserName = false
 
 const task_database = {};
 (function () {
@@ -144,9 +40,7 @@ const task_database = {};
             });
     }
 
-    function show_task(taskId, task) {
-        if (!task) return { success: false, message: 'Usuário inválido' }
-
+    function show_task(taskId) {
         let task_ref = firebase.database().ref('/tasks/' + taskId);
 
         return task_ref.get().then((snapshot) => {
@@ -163,6 +57,11 @@ const task_database = {};
     function show_all_task() {
         let task_ref = firebase.database().ref('/tasks');
 
+        // if(painelUserName){
+        //     console.log("AQUI", painelUserName);
+        //     task_ref = task_ref.equalTo(painelUserName, "userName")
+        // }
+
         return task_ref.get().then((snapshot) => {
             if (snapshot.exists()) {
                 return snapshot.val()
@@ -174,11 +73,10 @@ const task_database = {};
         });
     }
 
-    function completed(taskId, status = true)
-    {
+    function completed(taskId, timesFinished = 1,status = true) {
         let task_ref = firebase.database().ref('/tasks/' + taskId);
 
-        task_ref.update({completed: status})
+        task_ref.update({ completed: status, timesFinished })
             .then(function () {
                 return {
                     success: true,
@@ -191,7 +89,7 @@ const task_database = {};
                     success: false,
                     message: 'Erro ao atualizar o usuário'
                 }
-        });
+            });
     }
 
     function edit_task(taskId, task) {
@@ -211,7 +109,7 @@ const task_database = {};
                     success: false,
                     message: 'Erro ao atualizar o usuário'
                 }
-        });
+            });
     }
 
     function remove_task(taskId) {
@@ -245,25 +143,38 @@ $(function () {
     loadTasks()
 })
 
+function showTasksPainel(){
 
-function loadTasks(){
+    painelUserName = $("#painel-task option:selected").val() == "" ? false : $("#painel-task option:selected").val()
+    console.log(painelUserName);
+    loadTasks()
+}
+
+
+function loadTasks() {
     totalTasks = 0
     tasks = task_database.showAll()
     tasks.then((values) => {
+        console.log(values);
         $("#tasks-list").html('')
-        
-        if(values.success==false){
+
+        if (values.success == false) {
             $("#tasks-list").html(`
                 <p style="text-align: center;margin-top: 50px;"><b>Nenhuma tarefa criada</b></p>
             `)
         } else {
-            Object.keys(values).sort(function(a, b){
+            Object.keys(values).sort(function (a, b) {
                 return values[a].createdat > values[b].createdat;
             }).forEach(key => {
                 totalTasks += 1;
                 let task = values[key];
-    
-                $("#tasks-list").append(`
+
+                if(painelUserName &&  painelUserName != task.userName){
+                    return;
+                }
+
+                if (!task.completed) {
+                    $("#tasks-list").prepend(`
                     <div class="tasks-card ${task.completed ? 'task-completed' : ''}" id="task-item-${key}">
                         <div class="row">
                             <div class="col-md-10">
@@ -297,11 +208,53 @@ function loadTasks(){
                         </div>
                     </div>
                 `)
+                } else {
+                    $("#tasks-list").append(`
+                    <div class="tasks-card ${task.completed ? 'task-completed' : ''}" id="task-item-${key}">
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="tasks-header">
+                                    <h4 class="tasks-number">Nº ${task.number ?? 'sem número'}</h4>
+                                    <h1 class="tasks-title">${task.title}</h1>
+                                </div>
+                                <div class="tasks-body">
+                                    <p class="task-description">
+                                        ${task.description}
+                                    </p>
+                                </div>
+                                <div class="tasks-footer">
+                                    <b class="responsible-name">${task.userName}</b>
+                                </div>
+                            </div>
+    
+                            <div class="col-md-2 d-flex align-items-center">
+                                <button class="btn btn-primary"  id="taks-done-${key}" style="margin-right: 10px;" onclick="doneTask('${key}');">
+                                    <span class="material-icons">done</span>
+                                </button>
+    
+                                <button class="btn btn-success" id="taks-edit-${key}" style="margin-right: 10px;" onclick="editTask('${key}', '${task.title}', '${task.description}', '${task.userName}');" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    <span class="material-icons">edit</span>
+                                </button>
+    
+                                <button class="btn btn-danger" id="taks-delete-${key}" onclick="removeTask('${key}');">
+                                    <span class="material-icons">delete</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                `)
+                }
 
-                if(task.completed){
-                    $(`#taks-done-${key}`).hide();
-                    $(`#taks-edit-${key}`).hide();
-                    $(`#taks-delete-${key}`).hide();
+
+
+                if (task.completed) {
+                    if(task.timesFinished > 2){
+                        $(`#taks-done-${key}`).hide();
+                        $(`#taks-edit-${key}`).hide();
+                    } else {
+                        $(`#taks-done-${key}`).show();
+                        $(`#taks-edit-${key}`).show();
+                    }
                 }
             });
         }
@@ -309,38 +262,50 @@ function loadTasks(){
     })
 }
 
-$("#createTaks").submit(function(e){
+$("#createTaks").submit(function (e) {
     e.prenvetDefault();
 })
 
-function doneTask(taskId){
-    task_database.completed(taskId, true)
-    $(`#task-item-${taskId}`).hide("slow");
-    loadTasks()
+function getTask(taskId){
+    return task_database.show(taskId)
 }
 
-function createTaks(){
+function doneTask(taskId) {
+    getTask(taskId).then((task) => {
+        if(task.completed){
+            task_database.completed(taskId, task.timesFinished, false)
+        } else {
+            task_database.completed(taskId, task.timesFinished += 1,true)
+            $(`#task-item-${taskId}`).hide("slow");
+        }
+        
+        loadTasks()
+    })
+}
+
+function createTaks() {
 
     tasks = task_database.showAll()
     tasks.then((values) => {
-        
+
         task = {
             number: totalTasks + 1,
             userName: $("#task-username-input option:selected").val(),
             title: $("#task-title-input").val(),
             description: $("#task-description-input").val(),
             completed: false,
-            createdat: firebase.database.ServerValue.TIMESTAMP,	
+            timesFinished: 0,
+            createdat: firebase.database.ServerValue.TIMESTAMP,
         }
-        
+
         task_database.new(task);
         loadTasks()
     })
 
-    
+
 }
 
-function editTask(id, title, description, userName){
+function editTask(id, title, description, userName) {
     $("#task-id-input").val(id)
     $("#task-title-input").val(title)
     $("#task-description-input").val(description)
@@ -350,7 +315,7 @@ function editTask(id, title, description, userName){
     $("#exampleModalLabel").html("Atualizar tarefa")
 }
 
-function newTask(){
+function newTask() {
     $("#task-id-input").val("")
     $("#task-title-input").val("")
     $("#task-description-input").val("")
@@ -360,21 +325,22 @@ function newTask(){
     $("#exampleModalLabel").html("Nova tarefa")
 }
 
-function updateTaks(){
+function updateTaks() {
     task = {
         userName: $("#task-username-input option:selected").val(),
         title: $("#task-title-input").val(),
         description: $("#task-description-input").val(),
         completed: false,
-        createdat: firebase.database.ServerValue.TIMESTAMP,	
+        createdat: firebase.database.ServerValue.TIMESTAMP,
     }
     task_database.edit($("#task-id-input").val(), task)
     loadTasks()
 }
 
-function removeTask(key){
-    if(confirm("Você realmente quer excluir essa tarefa?")){
+function removeTask(key) {
+    if (confirm("Você realmente quer excluir essa tarefa?")) {
         task_database.remove(key)
+        $(`#task-item-${key}`).hide("slow");
         loadTasks()
     }
 }
